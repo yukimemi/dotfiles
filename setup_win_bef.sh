5ead48126d8dfc0d980d13661af2212470f84b94
yukimemi <yukimemi@gmail.com>
2014-02-15 18:58:38 +0900

    fixed if sentence

--- setup_win_bef.sh
+++ setup_win_bef.sh
@@ -3,20 +3,26 @@ git submodule init
 git submodule update
 
 # oh-my-zsh
-[ ! -d "${HOME}/.oh-my-zsh" ] && git clone git@github.com:yukimemi/oh-my-zsh.git ${HOME}/.oh-my-zsh
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+    git clone git@github.com:yukimemi/oh-my-zsh.git ${HOME}/.oh-my-zsh
+fi
 
 # vimperator
 VIMPERATOR_DIR="vimperator"
 VIMPERATOR_FILE="_vimperatorrc"
 cd vimperator
-[ ! -d ${HOME}/${VIMPERATOR_DIR}/plugin ] && mkdir -p ${HOME}/${VIMPERATOR_DIR}/plugin
+if [ ! -d ${HOME}/${VIMPERATOR_DIR}/plugin ]; then
+    mkdir -p ${HOME}/${VIMPERATOR_DIR}/plugin
+fi
 git clone git://github.com/caisui/vimperator.git ${HOME}/${VIMPERATOR_DIR}/caisui
 git clone git://gist.github.com/377348.git ${HOME}/${VIMPERATOR_DIR}/377348
 git clone git://github.com/vimpr/vimperator-plugins.git ${HOME}/${VIMPERATOR_DIR}/vimperator-plugins
 git clone git://github.com/vimpr/vimperator-rc.git ${HOME}/${VIMPERATOR_DIR}/vimperator-rc
 
 # scripts git clone
-[ ! -d ${HOME}/bin ] && mkdir ${HOME}/bin
+if [ ! -d ${HOME}/bin ]; then
+    mkdir ${HOME}/bin
+fi
 cd ${HOME}/bin
 git clone git@bitbucket.org:yukimemi/scripts.git
 
