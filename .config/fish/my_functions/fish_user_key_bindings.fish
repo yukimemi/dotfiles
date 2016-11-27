021120c4c64d5625a15402470d832924e9254303
yukimemi <yukimemi@gmail.com>
2016-11-27 13:27:17 +0900

    :+1: Add __filter_command_nvim and mvim function for Vim.

--- .config/fish/my_functions/fish_user_key_bindings.fish
+++ .config/fish/my_functions/fish_user_key_bindings.fish
@@ -4,7 +4,7 @@ function fish_user_key_bindings
   bind -M insert \cf accept-autosuggestion
   bind -M insert \cn down-or-search
   bind -M insert \cp up-or-search
-  # TODO: Not enable.
+  # TODO: Not work.
   bind -M insert \c\^ __cdup
 
   # tmux.
@@ -25,18 +25,4 @@ function fish_user_key_bindings
   bind -M insert \cs __filter_command_shell_snippets
   bind sul __filter_command_history_execute
 
-  ### fzf ###
-  bind \ct '__fzf_ctrl_t'
-  bind \cr '__fzf_ctrl_r'
-  bind \cx '__fzf_ctrl_x'
-  bind \ec '__fzf_alt_c'
-  bind \eC '__fzf_alt_shift_c'
-  if bind -M insert >/dev/null ^/dev/null
-    bind -M insert \ct '__fzf_ctrl_t'
-    bind -M insert \cr '__fzf_ctrl_r'
-    bind -M insert \cx '__fzf_ctrl_x'
-    bind -M insert \ec '__fzf_alt_c'
-    bind -M insert \eC '__fzf_alt_shift_c'
-  end
-  ### fzf ###
 end
