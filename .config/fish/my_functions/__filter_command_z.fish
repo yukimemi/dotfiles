329438c5979e682cf0ebc69c8066803394fde41d
yukimemi <yukimemi@gmail.com>
2017-01-12 19:04:36 +0900

    :+1: Add __filter_command_kill func for fish.

--- .config/fish/my_functions/__filter_command_z.fish
+++ .config/fish/my_functions/__filter_command_z.fish
@@ -1,5 +1,5 @@
 function __filter_command_z
-  z -lr | awk '{ print $2 }' | __filter_command | read -l line
+  z -lt | awk '{ print $2 }' | __filter_command | read -l line
   and echo "
   ------
   Change directory: $line
