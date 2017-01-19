d5b2532ca82f6872348d4138898e7fb21dd8a14b
yukimemi <yukimemi@gmail.com>
2017-01-20 06:53:55 +0900

    :sparkles: Add __filter_command_git_select_branch and __filter_command_rm for fish.

--- .config/fish/my_functions/__filter_command_kill.fish
+++ .config/fish/my_functions/__filter_command_kill.fish
@@ -1,9 +1,7 @@
 function __filter_command_kill
   ps -ef | __filter_command | read -l line
-  and echo "
-  ------
-  KILL: $line
-  ------"
+  and __echo "KILL" $line
   and kill -9 (echo $line | awk '{ print $2 }')
+  commandline -f repaint
 end
 
