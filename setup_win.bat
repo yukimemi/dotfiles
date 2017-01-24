47276f9b9531be32edadc643124871fd045f7303
yukimemi <yukimemi@gmail.com>
2017-01-25 07:07:04 +0900

    :+1: Add init.cmd for windows cmd.

--- setup_win.bat
+++ setup_win.bat
@@ -1,5 +1,5 @@
 @echo off
-REM {{{ env settings ===========================================
+REM env settings =========================================== {{{1
 set THIS_FILE_PASS=%~dp0
 set THIS_FILE_NAME=%~nx0
 set tm=%time: =0%
@@ -23,9 +23,9 @@ if "%os%"=="XP" (
     set RDCMD=rd /s /q
 )
 
-REM =====================================================}}}
+REM =====================================================
 
-REM vimperator
+REM vimperator {{{1
 if exist "%USERPROFILE%\_vimperatorrc" del "%USERPROFILE%\_vimperatorrc"
 %LINKCMD_F% "%USERPROFILE%\_vimperatorrc" "%THIS_FILE_PASS%.vimperatorrc"
 if not exist "%USERPROFILE%\vimperator\plugin" mkdir "%USERPROFILE%\vimperator\plugin"
@@ -34,7 +34,7 @@ git clone https://github.com/vimpr/vimperator-colors.git %USERPROFILE%\vimperato
 if exist "%USERPROFILE%\vimperator\vimppm" %RDCMD% "%USERPROFILE%\vimperator\vimppm"
 git clone https://github.com/cd01/vimppm.git %USERPROFILE%\vimperator\vimppm
 
-REM vim
+REM vim {{{1
 if exist "%USERPROFILE%\.vimrc" del "%USERPROFILE%\.vimrc"
 REM %LINKCMD_F% "%USERPROFILE%\.vimrc" "%THIS_FILE_PASS%.vimrc"
 %LINKCMD_F% "%USERPROFILE%\.vimrc" "%THIS_FILE_PASS%.config\nvim\init.vim"
@@ -43,17 +43,21 @@ if exist "%USERPROFILE%\.gvimrc" del "%USERPROFILE%\.gvimrc"
 if exist "%USERPROFILE%\.vim" %RDCMD% "%USERPROFILE%\.vim"
 %LINKCMD_D% "%USERPROFILE%\.vim" "%THIS_FILE_PASS%.config\nvim"
 
-REM zsh
+REM cmd {{{1
+if exist "%USERPROFILE%\.init.cmd" del "%USERPROFILE%\.init.cmd"
+%LINKCMD_F% "%USERPROFILE%\.init.cmd" "%THIS_FILE_PASS%win\cmd\init.cmd"
+
+REM zsh {{{1
 REM if exist "%CYGWIN_HOME%\.zshrc" rename "%CYGWIN_HOME%\.zshrc" ".zshrc.bak_%TODAY%"
 REM %LINKCMD_F% "%CYGWIN_HOME%\.zshrc" "%CYGWIN_HOME%\.oh-my-zsh\templates\zshrc.zsh-template"
 REM if exist "%CYGWIN_HOME%\.zshenv" rename "%CYGWIN_HOME%\.zshenv" ".zshenv.bak_%TODAY%"
 REM %LINKCMD_F% "%CYGWIN_HOME%\.zshenv" "%THIS_FILE_PASS%zsh\.zshenv"
 
-REM screenrc
+REM screenrc {{{1
 REM if exist "%CYGWIN_HOME%\.screenrc" rename "%CYGWIN_HOME%\.screenrc" ".screenrc.bak_%TODAY%"
 REM %LINKCMD_F% "%CYGWIN_HOME%\.screenrc" "%THIS_FILE_PASS%.screenrc"
 
-REM mintty
+REM mintty {{{1
 REM if exist "%CYGWIN_HOME%\.minttyrc" rename "%CYGWIN_HOME%\.minttyrc" ".minttyrc.bak_%TODAY%"
 REM %LINKCMD_F% "%CYGWIN_HOME%\.minttyrc" "%THIS_FILE_PASS%.minttyrc"
 REM if exist "%CYGWIN_HOME%\.inputrc" rename "%CYGWIN_HOME%\.inputrc" ".inputrc.bak_%TODAY%"
