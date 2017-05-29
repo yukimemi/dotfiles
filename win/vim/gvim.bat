be9dccc78a573cc10b57026e27a7947b3c3646ee
yukimemi <yukimemi@gmail.com>
2017-05-29 14:50:06 +0900

    :bug: Fix gvim.bat for space file name.

--- win/vim/gvim.bat
+++ win/vim/gvim.bat
@@ -4,6 +4,6 @@ if "%*"=="" (
   start "" gvim.exe
 ) else (
   REM start "" gvim.exe --noplugin --remote-tab-silent %*
-  start "" gvim.exe --remote-tab-silent %*
+  start "" gvim.exe --remote-tab-silent "%*"
 )
 exit
