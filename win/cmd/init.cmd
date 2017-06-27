a6866b77fa915ef0253700ad0a28f383e30d6454
yukimemi <yukimemi@gmail.com>
2017-06-27 13:00:48 +0900

    :sparkles: Move after/plugin settings to dein.toml.

--- win/cmd/init.cmd
+++ win/cmd/init.cmd
@@ -16,6 +16,7 @@ doskey e=gvim --remote-tab-silent $*
 doskey b=cd ..
 doskey o=start $*
 doskey vup=vim -c "PlugUpdate"
+doskey dup=vim -c "silent! call dein#update() | Capture Dein log"
 
 rem Git
 doskey s=git status
