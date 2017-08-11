859d302ff010e20859b11780df2f92db654c4b96
yukimemi <yukimemi@gmail.com>
2017-08-11 15:29:07 +0900

    :sparkles: Add prettier/vim-prettier plugin for vim.

--- .config/vimfx/config.js
+++ .config/vimfx/config.js
@@ -1,133 +1,128 @@
-const {
-  classes: Cc,
-  interfaces: Ci,
-  utils: Cu
-} = Components;
-const gClipboardHelper = Cc['@mozilla.org/widget/clipboardhelper;1']
-  .getService(Ci.nsIClipboardHelper);
-const {
-  Preferences
-} = Cu.import('resource://gre/modules/Preferences.jsm', {});
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+const gClipboardHelper = Cc["@mozilla.org/widget/clipboardhelper;1"].getService(
+  Ci.nsIClipboardHelper
+);
+const { Preferences } = Cu.import("resource://gre/modules/Preferences.jsm", {});
 
-const {
-  sendKey
-} = Cu.import(`${__dirname}/shared.js?${Math.random()}`, {});
+const { sendKey } = Cu.import(`${__dirname}/shared.js?${Math.random()}`, {});
 
 const FIREFOX_PREFS = {
-  'browser.startup.page': 3,
-  'browser.tabs.animate': false,
-  'browser.search.suggest.enabled': true,
-  'browser.urlbar.suggest.searches': true,
-  'browser.urlbar.maxRichResults': 20,
-  'browser.tabs.remote.force-enable': true,
-  'dom.ipc.processCount': 4
+  "browser.startup.page": 3,
+  "browser.tabs.animate": false,
+  "browser.search.suggest.enabled": true,
+  "browser.urlbar.suggest.searches": true,
+  "browser.urlbar.maxRichResults": 20,
+  "browser.tabs.remote.force-enable": true,
+  "dom.ipc.processCount": 4
 };
 
 const VIMFX_PREFS = {
-  'prevent_autofocus': true
+  prevent_autofocus: true
 };
 
 const MAPPINGS = {
-  'copy_current_url': '',
-  'go_home': '',
-  'stop': '<c-escape>',
-  'stop_all': 'a<c-escape>',
+  copy_current_url: "",
+  go_home: "",
+  stop: "<c-escape>",
+  stop_all: "a<c-escape>",
 
-  'scroll_half_page_down': '<c-f>',
-  'scroll_half_page_up': '<c-b>',
-  'mark_scroll_position': 'mm',
-  'scroll_to_mark': 'gm',
+  scroll_half_page_down: "<c-f>",
+  scroll_half_page_up: "<c-b>",
+  mark_scroll_position: "mm",
+  scroll_to_mark: "gm",
 
-  'tab_new': 't',
-  'tab_new_after_current': 'T',
-  'tab_close': 'd x',
-  'tab_restore': 'u',
-  'tab_restore_list': 'U',
-  'tab_select_previous': '<c-h>',
-  'tab_select_next': '<c-l>',
-  'tab_select_first_non_pinned': '^',
-  'tab_select_last': '$',
+  tab_new: "t",
+  tab_new_after_current: "T",
+  tab_close: "d x",
+  tab_restore: "u",
+  tab_restore_list: "U",
+  tab_select_previous: "<c-h>",
+  tab_select_next: "<c-l>",
+  tab_select_first_non_pinned: "^",
+  tab_select_last: "$",
 
-  'enter_mode_ignore': 'I',
-  'quote': 'i',
+  enter_mode_ignore: "I",
+  quote: "i",
 
-  'custom.mode.normal.search_selected_text': 's',
-  'custom.mode.normal.copy_url': 'yy',
-  'custom.mode.normal.copy_as_markdown': 'ym',
-  'custom.mode.normal.click_toolbar_pocket': 'mp',
-  'custom.mode.normal.view_source': 'gf',
-  'custom.mode.normal.send_up': '<force><c-p>',
-  'custom.mode.normal.send_down': '<force><c-n>',
+  "custom.mode.normal.search_selected_text": "s",
+  "custom.mode.normal.copy_url": "yy",
+  "custom.mode.normal.copy_as_markdown": "ym",
+  "custom.mode.normal.click_toolbar_pocket": "mp",
+  "custom.mode.normal.view_source": "gf",
+  "custom.mode.normal.send_up": "<force><c-p>",
+  "custom.mode.normal.send_down": "<force><c-n>"
 };
 
-const {
-  commands
-} = vimfx.modes.normal;
+const { commands } = vimfx.modes.normal;
 
-function _sendKey(key, {
-  vim,
-  event
-}) {
+function _sendKey(key, { vim, event }) {
   if (vim.isUIEvent(event)) {
     sendKey(vim.window, key);
   } else {
-    vimfx.send(vim, 'sendKey', {
+    vimfx.send(vim, "sendKey", {
       key
     });
   }
 }
 
 const CUSTOM_COMMANDS = [
-  [{
-    name: 'search_selected_text',
-    description: 'Search for the selected text'
-  }, ({
-    vim
-  }) => {
-    vimfx.send(vim, 'getSelection', true, selection => {
-      if (selection != '') {
-        vim.window.switchToTabHavingURI(`https://www.google.co.jp/search?q=${selection}`, true);
-      }
-    });
-  }],
-  [{
-    name: 'copy_as_markdown',
-    description: 'Copy title and url as Markdown',
-    category: 'location',
-    order: commands.copy_current_url.order + 2
-  }, ({
-    vim
-  }) => {
-    let url = vim.window.gBrowser.selectedBrowser.currentURI.spec;
-    let title = vim.window.gBrowser.selectedBrowser.contentTitle;
-    let s = `[${title}](${url})`;
-    gClipboardHelper.copyString(s);
-    vim.notify(`Copied to clipboard: ${s}`);
-  }],
-  [{
-    name: 'copy_url',
-    description: 'Copy the current url',
-    category: 'location',
-    order: commands.copy_current_url.order + 1
-  }, ({
-    vim
-  }) => {
-    let url = vim.window.gBrowser.selectedBrowser.currentURI.spec;
-    gClipboardHelper.copyString(url);
-    vim.notify(`Copied to clipboard: ${url}`);
-  }],
-  [{
-    name: 'click_toolbar_pocket',
-    description: 'Click toolbar button [Pocket]'
-  }, ({
-    vim
-  }) => {
-    vim.window.document.getElementById('pocket-button').click();
-  }],
   [
     {
-      name: 'view_source',
-      description: 'View source'
+      name: "search_selected_text",
+      description: "Search for the selected text"
+    },
+    ({ vim }) => {
+      vimfx.send(vim, "getSelection", true, selection => {
+        if (selection != "") {
+          vim.window.switchToTabHavingURI(
+            `https://www.google.co.jp/search?q=${selection}`,
+            true
+          );
+        }
+      });
+    }
+  ],
+  [
+    {
+      name: "copy_as_markdown",
+      description: "Copy title and url as Markdown",
+      category: "location",
+      order: commands.copy_current_url.order + 2
+    },
+    ({ vim }) => {
+      let url = vim.window.gBrowser.selectedBrowser.currentURI.spec;
+      let title = vim.window.gBrowser.selectedBrowser.contentTitle;
+      let s = `[${title}](${url})`;
+      gClipboardHelper.copyString(s);
+      vim.notify(`Copied to clipboard: ${s}`);
+    }
+  ],
+  [
+    {
+      name: "copy_url",
+      description: "Copy the current url",
+      category: "location",
+      order: commands.copy_current_url.order + 1
+    },
+    ({ vim }) => {
+      let url = vim.window.gBrowser.selectedBrowser.currentURI.spec;
+      gClipboardHelper.copyString(url);
+      vim.notify(`Copied to clipboard: ${url}`);
+    }
+  ],
+  [
+    {
+      name: "click_toolbar_pocket",
+      description: "Click toolbar button [Pocket]"
+    },
+    ({ vim }) => {
+      vim.window.document.getElementById("pocket-button").click();
+    }
+  ],
+  [
+    {
+      name: "view_source",
+      description: "View source"
     },
     ({ vim }) => {
       let url = vim.window.gBrowser.selectedBrowser.currentURI.spec;
@@ -140,17 +135,19 @@ const CUSTOM_COMMANDS = [
       vim.window.gBrowser.loadURI(`${url}`);
     }
   ],
-  [{
-      name: 'send_up',
-      description: 'Send the <up> key'
+  [
+    {
+      name: "send_up",
+      description: "Send the <up> key"
     },
-    _sendKey.bind(null, 'up')
+    _sendKey.bind(null, "up")
   ],
-  [{
-      name: 'send_down',
-      description: 'Send the <down> key'
+  [
+    {
+      name: "send_down",
+      description: "Send the <down> key"
     },
-    _sendKey.bind(null, 'down')
+    _sendKey.bind(null, "down")
   ]
 ];
 
@@ -163,7 +160,7 @@ CUSTOM_COMMANDS.forEach(([options, fn]) => {
 });
 
 Object.entries(MAPPINGS).forEach(([cmd, key]) => {
-  if (!cmd.includes('.')) {
+  if (!cmd.includes(".")) {
     cmd = `mode.normal.${cmd}`;
   }
   vimfx.set(cmd, key);
