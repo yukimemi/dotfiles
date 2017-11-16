9e6f378d9434b3cb40d1e6117b08b672dfebf2b4
yukimemi <yukimemi@gmail.com>
2017-11-16 14:31:49 +0900

    tmp

--- win/vim/gvim.bat
+++ win/vim/gvim.bat
@@ -3,8 +3,6 @@ if "%*"=="" (
   REM start "" gvim.exe --noplugin
   start "" gvim.exe
 ) else (
-  REM start "" gvim.exe --noplugin --remote-tab-silent %*
-  REM start "" gvim.exe --remote-tab-silent "%*"
-  start "" gvim.exe --remote-silent "%*"
+  start "" gvim.exe --remote-tab-silent "%*"
 )
 exit
