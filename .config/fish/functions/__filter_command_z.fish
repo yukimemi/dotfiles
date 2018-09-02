5c8cade96f773f1463eeecf03c6d4cb3ee53487a
yukimemi <yukimemi@gmail.com>
2018-09-03 00:27:39 +0900

    :bug: Fix __filter_command_z.

--- .config/fish/functions/__filter_command_z.fish
+++ .config/fish/functions/__filter_command_z.fish
@@ -1,5 +1,5 @@
 function __filter_command_z
-  cat ~/.z | while read -l line
+  tac ~/.z | while read -l line
     echo $line
   end | __filter_command | read -l dir
   and __echo "Change directory" $dir
