c15c14c92f236ed7ed645d62521823991832f1f1
yukimemi <yukimemi@gmail.com>
2018-09-06 05:41:57 +0900

    :bug: Fix how to get fnm_version.

--- .config/fish/functions/__fnm_version.fish
+++ .config/fish/functions/__fnm_version.fish
@@ -1,5 +1,5 @@
 function __fnm_version
   if type -q fnm
-      echo (set_color green)" N: ["(fnm ls | grep '•' | sed 's/ • //')"]"(set_color normal)
+    echo (set_color green)" N: ["(__fnm_version_which)"]"(set_color normal)
   end
 end
