d3284a388a61358b25bcb58240be6b28840ba9a9
yukimemi <yukimemi@gmail.com>
2018-12-08 22:50:04 +0900

    :+1: Change Tweetvim command.

--- .config/fish/functions/__history-merge.fish
+++ .config/fish/functions/__history-merge.fish
@@ -1,5 +1,5 @@
 # https://qiita.com/yoshiori/items/f1c01dd94bb5f0489cf6
-function history-merge --on-event fish_preexec
+function __history-merge --on-event fish_preexec
   history --save
   history --merge
 end
