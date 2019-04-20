284ed2e757046259cf71ef9b8aa92f958821b0e6
yukimemi <yukimemi@gmail.com>
2019-04-20 17:35:29 +0900

    :+1: Add git-messenger plugin.

--- etc/init/ubuntu/install-neovim.sh
+++ etc/init/ubuntu/install-neovim.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt update
-sudo apt install -y python3 python3-pip
-sudo apt install -y neovim
+sudo apt install -y python-dev python-pip python3-dev python3-pip neovim
 pip3 install --upgrade pip
 pip3 install neovim
+
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
 
