7657d6430bda7f3a66e1f649d7009f37dcbac9a3
yukimemi <yukimemi@gmail.com>
2020-03-19 22:22:03 +0900

    :+1: Add gig command.

--- win/Microsoft.PowerShell_profile.ps1
+++ win/Microsoft.PowerShell_profile.ps1
@@ -28,6 +28,15 @@ function a {
 function gp {
   git pull --rebase
 }
+# git ignore for PowerShell v3
+Function gig {
+  param(
+    [Parameter(Mandatory=$true)]
+    [string[]]$list
+  )
+  $params = ($list | ForEach-Object { [uri]::EscapeDataString($_) }) -join ","
+  Invoke-WebRequest -Uri "https://www.gitignore.io/api/$params" | select -ExpandProperty content | Out-File -FilePath $(Join-Path -path $pwd -ChildPath ".gitignore") -Encoding ascii
+}
 
 # rhq.
 function rhl {
