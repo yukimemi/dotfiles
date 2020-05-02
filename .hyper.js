32d68aa4de4e470aa7da3a34fd9af10f19231c06
yukimemi <yukimemi@gmail.com>
2020-05-03 00:59:11 +0900

    :tada: Add default config.

--- .hyper.js
+++ .hyper.js
@@ -20,6 +20,12 @@ module.exports = {
     // font weight for bold characters: 'normal' or 'bold'
     fontWeightBold: "bold",
 
+    // line height as a relative unit
+    lineHeight: 1,
+
+    // letter spacing as a relative unit
+    letterSpacing: 0,
+
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: "rgba(248,28,229,0.8)",
 
