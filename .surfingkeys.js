d7c4e78e90ff270aa8d010fc5f8195f685224180
yukimemi <yukimemi@gmail.com>
2020-08-15 22:47:25 +0900

    :zap: Update surfingkeys config.

--- .surfingkeys.js
+++ .surfingkeys.js
@@ -1,24 +1,106 @@
 // j/k scrolls in one step with size as 70, you could change it as below:
 settings.smoothScroll = true;
 settings.scrollStepSize = 140;
+settings.hintAlign = "left";
 
 // mappings.
 // History
 map("H", "S");
 map("L", "D");
 
-// an example to create a new mapping `ctrl-y`
-mapkey("<Ctrl-y>", "Show me the money", function() {
-  Front.showPopup(
-    "a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close)."
-  );
+// open URL from clipboard
+mapkey("p", "#3Open URL from clipboard", () => {
+  Clipboard.read((response) => {
+    window.location.href = response.data;
+  });
+});
+mapkey("P", "#3Open URL from clipboard in a new tab", () => {
+  Clipboard.read((response) => {
+    window.open(response.data);
+  });
+});
+mapkey(
+  "gE",
+  "#12 go Extensions - Open Chrome extensions Shortcut setting",
+  function () {
+    tabOpenLink("chrome://extensions/shortcuts");
+  }
+);
+
+// qmarks
+var overlayedGlobalMarks = {
+  "1": "https://mail.google.com/mail/u/0",
+  "2": "http://www.google.com/calendar",
+  "9": [
+    "https://b.hatena.ne.jp",
+    "https://qiita.com",
+    "https://www.reddit.com/r/vim",
+  ],
+  "3": "https://www.google.com/contacts/#contacts",
+  g: "https://plus.google.com/u/0/",
+  r: "https://www.reddit.com/r/vim",
+  m: "https://maps.google.com/",
+  u: "https://www.youtube.com/feed/subscriptions",
+  h: "https://github.com/",
+  f: "https://www.facebook.com",
+};
+
+mapkey("gn", "#10Jump to vim-like mark in new tab.", function (mark) {
+  var priorityURLs = overlayedGlobalMarks[mark];
+  if (priorityURLs === undefined) {
+    // fallback to Surfingkeys default jump
+    Normal.jumpVIMark(mark, true);
+    return;
+  }
+  if (typeof priorityURLs == typeof "") {
+    priorityURLs = [priorityURLs];
+  }
+  for (var url of priorityURLs) {
+    var markInfo = {
+      url: url,
+      scrollLeft: 0,
+      scrollTop: 0,
+    };
+    markInfo.tab = {
+      tabbed: true,
+      active: true,
+    };
+    RUNTIME("openLink", markInfo);
+  }
 });
 
-// an example to replace `T` with `gt`, click `Default mappings` to see how `T` works.
-map("gt", "T");
+mapkey("go", "#10Jump to vim-like mark in current tab.", function (mark) {
+  var priorityURLs = overlayedGlobalMarks[mark];
+  if (priorityURLs === undefined) {
+    // fallback to Surfingkeys default jump
+    Normal.jumpVIMark(mark, true);
+    return;
+  }
+  if (typeof priorityURLs == typeof "") {
+    priorityURLs = [priorityURLs];
+  }
+  for (var url of priorityURLs) {
+    var markInfo = {
+      url: url,
+      scrollLeft: 0,
+      scrollTop: 0,
+    };
+    markInfo.tab = {
+      tabbed: false,
+      active: false,
+    };
+    RUNTIME("openLink", markInfo);
+  }
+});
 
-// an example to remove mapkey `Ctrl-i`
-unmap("<Ctrl-i>");
+addSearchAlias("y", "youtube", "https://www.youtube.com/results?search_query=");
+addSearchAlias("a", "Amazon", "http://www.amazon.co.jp/s/?field-keywords=");
+addSearchAlias(
+  "i",
+  "Google Img",
+  "https://www.google.com.ar/search?site=imghp&tbm=isch&source=hp&biw=1478&bih=740&q="
+);
+addSearchAlias("map", "Google Maps", "https://www.google.com.ar/maps/search/");
 
 // https://www.ncaq.net/2018/12/09/16/13/27/
 mapkey("<Ctrl-g>", "google translate", () => {
@@ -48,7 +130,7 @@ function mapsitekey(domainRegex, key, desc, f, opts = {}) {
   const o = Object.assign(
     {},
     {
-      leader: siteleader
+      leader: siteleader,
     },
     opts
   );
@@ -60,7 +142,7 @@ function mapsitekeys(d, maps, opts = {}) {
   const domainRegex = new RegExp(
     `^http(s)?://(([a-zA-Z0-9-_]+\\.)*)(${domain})(/.*)?`
   );
-  maps.forEach(map => {
+  maps.forEach((map) => {
     const [key, desc, f, subOpts = {}] = map;
     mapsitekey(domainRegex, key, desc, f, Object.assign({}, opts, subOpts));
   });
@@ -81,10 +163,10 @@ mapsitekeys(
       () =>
         window.location.assign(
           "https://www.youtube.com/feed/subscriptions?flow=2"
-        )
+        ),
     ],
     ["F", "Toggle fullscreen", ytFullscreen],
-    ["<Space>", "Play/pause", Hint(".ytp-play-button")]
+    ["<Space>", "Play/pause", Hint(".ytp-play-button")],
   ],
   { leader: "" }
 );
