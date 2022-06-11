69a34d5be018fbb30063eda3a60fb38cb491957a
yukimemi <yukimemi@gmail.com>
2022-06-11 21:16:19 +0900

    :zap: Add some hyper plugins.

--- .hyper.js
+++ .hyper.js
@@ -130,6 +130,9 @@ module.exports = {
         // set to true to preserve working directory when creating splits or tabs
         preserveCWD: true,
         // for advanced config flags please refer to https://hyper.is/#cfg
+
+        // hyper-opacity.
+        opacity: 0.95,
     },
     // a list of plugins to fetch and install from npm
     // format: [@org/]project[#version]
@@ -137,7 +140,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: [],
+    plugins: ['hyperpower', 'hyper-rose-pine', 'hyper-opacity', 'hyper-bloodbath', 'git-falcon9', 'hyperborder'],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
