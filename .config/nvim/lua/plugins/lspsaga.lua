4984147ab8f58fa070bd169eb695be95b2cbd96b
yukimemi <yukimemi@gmail.com>
2023-01-09 20:50:04 +0900

    :zap: Use lspsaga.

--- .config/nvim/lua/plugins/lspsaga.lua
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -2,7 +2,7 @@ return {
   "glepnir/lspsaga.nvim",
   branch = "main",
 
-  enabled = false,
+  enabled = true,
 
   event = "BufReadPre",
 
@@ -14,60 +14,57 @@ return {
     local saga = require('lspsaga')
     saga.init_lsp_saga()
 
-    local keymap = vim.keymap.set
-
     -- Lsp finder find the symbol definition implement reference
     -- if there is no implement it will hide
     -- when you use action in finder like open vsplit then you can
     -- use <C-t> to jump back
-    keymap("n", "gH", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+    vim.keymap.set("n", "gH", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
 
     -- Code action
-    keymap({"n", "v"}, "<space>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
+    vim.keymap.set({ "n", "v" }, "<space>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
 
     -- Rename
-    keymap("n", "<space>rn", "<cmd>Lspsaga rename<CR>", { silent = true })
+    vim.keymap.set("n", "<space>rn", "<cmd>Lspsaga rename<CR>", { silent = true })
 
     -- Peek Definition
     -- you can edit the definition file in this flaotwindow
     -- also support open/vsplit/etc operation check definition_action_keys
     -- support tagstack C-t jump back
-    keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
+    vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
 
     -- Show line diagnostics
-    keymap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
 
     -- Show cursor diagnostics
-    keymap("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
 
     -- Diagnostic jump can use `<c-o>` to jump back
-    keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
-    keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
+    vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
+    vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
 
     -- Only jump to error
-    keymap("n", "[E", function()
+    vim.keymap.set("n", "[e", function()
       require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
     end, { silent = true })
-    keymap("n", "]E", function()
+    vim.keymap.set("n", "]e", function()
       require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
     end, { silent = true })
 
     -- Outline
-    keymap("n","<space>o", "<cmd>LSoutlineToggle<CR>",{ silent = true })
+    vim.keymap.set("n", "<space>o", "<cmd>LSoutlineToggle<CR>", { silent = true })
 
     -- Hover Doc
-    keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+    vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
 
     -- Float terminal
-    keymap("n", "<A-d>", "<cmd>Lspsaga open_floaterm<CR>", { silent = true })
+    vim.keymap.set("n", "<A-d>", "<cmd>Lspsaga open_floaterm<CR>", { silent = true })
 
     -- if you want to pass some cli command into a terminal you can do it like this
     -- open lazygit in lspsaga float terminal
-    keymap("n", "<A-d>", "<cmd>Lspsaga open_floaterm lazygit<CR>", { silent = true })
+    vim.keymap.set("n", "<A-d>", "<cmd>Lspsaga open_floaterm lazygit<CR>", { silent = true })
 
     -- close floaterm
-    keymap("t", "<A-d>", [[<C-\><C-n><cmd>Lspsaga close_floaterm<CR>]], { silent = true })
+    vim.keymap.set("t", "<A-d>", [[<C-\><C-n><cmd>Lspsaga close_floaterm<CR>]], { silent = true })
 
   end,
 }
-
--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -12,9 +12,7 @@ local M = {
       "williamboman/mason.nvim",
       cmd = "Mason",
     },
-    {
-      "williamboman/mason-lspconfig.nvim",
-    },
+    "williamboman/mason-lspconfig.nvim",
     {
       "hrsh7th/cmp-nvim-lsp",
       enabled = vim.g.plugin_use_cmp,
@@ -30,10 +28,18 @@ local M = {
         })
       end,
     },
-    "folke/neodev.nvim",
+    {
+      "folke/neodev.nvim",
+      config = function()
+        require("neodev").setup()
+      end,
+    },
     {
       "folke/neoconf.nvim",
       cmd = "Neoconf",
+      config = function()
+        require("neoconf").setup()
+      end,
     },
   },
 }
@@ -49,13 +55,13 @@ function M.config()
     local opts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set("n", "<space>e", vim.diagnostic.open_float,
       vim.tbl_extend("force", opts, { desc = "Open diagnostic on float" }))
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Diagnostic prev" }))
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Diagnostic next" }))
+    -- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Diagnostic prev" }))
+    -- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Diagnostic next" }))
     vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist,
       vim.tbl_extend("force", opts, { desc = "Diagnostic to loclist" }))
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "Declaration" }))
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Definition" }))
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover" }))
+    -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Definition" }))
+    -- vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover" }))
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "Implementation" }))
     vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, vim.tbl_extend("force", opts, { desc = "Signature help" }))
     vim.keymap.set("n", "<localleader>wa", vim.lsp.buf.add_workspace_folder,
@@ -67,8 +73,8 @@ function M.config()
     end, vim.tbl_extend("force", opts, { desc = "List workspace folders" }))
     vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition,
       vim.tbl_extend("force", opts, { desc = "Type definition" }))
-    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename" }))
-    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code action" }))
+    -- vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename" }))
+    -- vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code action" }))
     vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "References" }))
     vim.keymap.set('n', '<space>F', function() vim.lsp.buf.format({ async = true }) end,
       vim.tbl_extend("force", opts, { desc = "Format code" }))
