03173eb00e551f8bd23b62459101afe126c6b4da
yukimemi <yukimemi@gmail.com>
2023-01-15 18:42:12 +0900

    :zap: Remove unused settings.

--- .config/nvim/lua/config/gui.lua
+++ .config/nvim/lua/config/gui.lua
@@ -18,14 +18,5 @@ vim.api.nvim_create_autocmd("UIEnter", {
     if vim.fn.exists(":GuiWindowOpacity") > 0 then
       vim.cmd([[GuiWindowOpacity 0.9]])
     end
-    if vim.fn.exists(":GuiAdaptiveFont") > 0 then
-      vim.cmd([[GuiAdaptiveFont 1]])
-    end
-    if vim.fn.exists(":GuiAdaptiveColor") > 0 then
-      vim.cmd([[GuiAdaptiveColor 1]])
-    end
-    if vim.fn.exists(":GuiAdaptiveStyle") > 0 then
-      vim.cmd([[GuiAdaptiveStyle Fusion]])
-    end
   end,
 })
