e45c395ea9e2cc8d6beb45b3ddf8b1425b05cda9
yukimemi <yukimemi@gmail.com>
2023-05-22 17:34:47 +0900

    :zap: Add project.nvim plugin.

--- .config/nvim/denops/config/plugins/util.ts
+++ .config/nvim/denops/config/plugins/util.ts
@@ -19,7 +19,7 @@ export const util: Plug[] = [
       await globals.set(
         denops,
         "memolist_path",
-        await expand(denops, "~/.memolist"),
+        await expand(denops, "~/.memolist")
       );
       await globals.set(denops, "memolist_memo_suffix", "md");
       await globals.set(denops, "memolist_prompt_tags", 1);
@@ -29,6 +29,57 @@ export const util: Plug[] = [
       });
       await mapping.map(denops, "<leader>ml", "<cmd>MemoList<cr>", {
         mode: "n",
+      });
+    },
+  },
+  {
+    url: "ahmedkhalf/project.nvim",
+    after: async (denops: Denops) => {
+      await denops.call(`luaeval`, `require("project_nvim").setup(_A.param)`, {
+        param: {
+          // Manual mode doesn't automatically change your root directory, so you have
+          // the option to manually do so using `:ProjectRoot` command.
+          manual_mode: false,
+
+          // Methods of detecting the root directory. **"lsp"** uses the native neovim
+          // lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
+          // order matters: if one is not detected, the other is used as fallback. You
+          // can also delete or rearangne the detection methods.
+          detection_methods: ["lsp", "pattern"],
+
+          // All the patterns used to detect root dir, when **"pattern"** is in
+          // detection_methods
+          patterns: [
+            ".git",
+            "_darcs",
+            ".hg",
+            ".bzr",
+            ".svn",
+            "Makefile",
+            "package.json",
+          ],
+
+          // Table of lsp clients to ignore by name
+          // eg: { "efm", ... }
+          ignore_lsp: [],
+
+          // Don't calculate root dir on specific directories
+          // Ex: { "~/.cargo/*", ... }
+          exclude_dirs: [],
+
+          // Show hidden files in telescope
+          show_hidden: false,
+
+          // When set to false, you will get a message when project.nvim changes your
+          // directory.
+          silent_chdir: true,
+
+          // What scope to change the directory, valid options are
+          // * global (default)
+          // * tab
+          // * win
+          scope_chdir: "global",
+        },
       });
     },
   },
