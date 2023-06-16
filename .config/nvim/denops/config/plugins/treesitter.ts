fcb6cb757f86c902c06d792dc793fa2b0777721d
yukimemi <yukimemi@gmail.com>
2023-06-16 22:22:08 +0900

    :up: Update dvpm.

--- .config/nvim/denops/config/plugins/treesitter.ts
+++ .config/nvim/denops/config/plugins/treesitter.ts
@@ -1,4 +1,4 @@
-import type { Plug } from "https://deno.land/x/dvpm@1.1.0/mod.ts";
+import type { Plug } from "https://deno.land/x/dvpm@1.1.1/mod.ts";
 
 import { execute } from "https://deno.land/x/denops_std@v5.0.0/helper/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v5.0.0/function/mod.ts";
