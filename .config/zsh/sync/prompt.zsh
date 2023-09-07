f81cebdf53d30558397a3f70b2d8d9ce44ed8db0
yukimemi <yukimemi@gmail.com>
2023-09-07 23:43:14 +0900

    :zap: Use `starship` for the prompt.

--- .config/zsh/sync/prompt.zsh
+++ .config/zsh/sync/prompt.zsh
@@ -1,12 +1,14 @@
 # =============================================================================
 # File        : prompt.zsh
 # Author      : yukimemi
-# Last Change : 2023/09/07 23:12:36.
+# Last Change : 2023/09/07 23:27:02.
 # =============================================================================
 
-typeset -g POWERLEVEL9K_INSTANT_PROMPT=off
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+# typeset -g POWERLEVEL9K_INSTANT_PROMPT=off
+# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+# fi
+# [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+eval "$(starship init zsh)"
 
