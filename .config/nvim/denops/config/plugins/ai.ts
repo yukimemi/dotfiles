3b9b42b754f2d21b78a2f669b2c309e27752b8e2
yukimemi <yukimemi@gmail.com>
2024-01-13 13:17:07 +0900

    :zap: Add futago.vim settings.

--- .config/nvim/denops/config/plugins/ai.ts
+++ .config/nvim/denops/config/plugins/ai.ts
@@ -1,11 +1,17 @@
 // =============================================================================
 // File        : ai.ts
 // Author      : yukimemi
-// Last Change : 2024/01/08 23:06:40.
+// Last Change : 2024/01/13 13:16:47.
 // =============================================================================
 
 import type { Plug } from "https://deno.land/x/dvpm@3.8.1/mod.ts";
 import * as vars from "https://deno.land/x/denops_std@v5.2.0/variable/mod.ts";
+import {
+  GenerationConfig,
+  HarmBlockThreshold,
+  HarmCategory,
+  SafetySetting,
+} from "https://esm.sh/@google/generative-ai@0.1.3";
 
 export const ai: Plug[] = [
   { url: "https://github.com/kyoh86/denops-ollama.vim" },
@@ -13,9 +19,64 @@ export const ai: Plug[] = [
     url: "https://github.com/yukimemi/futago.vim",
     dst: "~/src/github.com/yukimemi/futago.vim",
     before: async ({ denops }) => {
-      await vars.g.set(denops, "futago_debug", false);
+      await vars.g.set(denops, "futago_debug", true);
       await vars.g.set(denops, "futago_chat_path", `~/.cache/nvim/futago/chat`);
       await vars.g.set(denops, "futago_log_file", `~/.cache/nvim/futago/log/futago.log`);
+      await vars.g.set(denops, "futago_model", "gemini-pro");
+      const safetySettings: SafetySetting[] = [
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+      ];
+      await vars.g.set(denops, "futago_safety_settings", safetySettings);
+      const gegerationConfig: GenerationConfig = {
+        temperature: 0.9,
+      };
+      await vars.g.set(denops, "futago_generation_config", gegerationConfig);
+    },
+    cache: {
+      enabled: false,
+      before: `
+        let g:futago_debug = v:true
+        let g:futago_chat_path = "~/.cache/nvim/futago/chat"
+        let g:futago_log_file = "~/.cache/nvim/futago/log/futago.log"
+        let g:futago_model = "gemini-pro"
+        let g:futago_safety_settings = [
+              \\ {
+              \\   "category": "HARM_CATEGORY_HATE_SPEECH",
+              \\   "threshold": "BLOCK_NONE",
+              \\ },
+              \\ {
+              \\   "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+              \\   "threshold": "BLOCK_NONE",
+              \\ },
+              \\ {
+              \\   "category": "HARM_CATEGORY_HARASSMENT",
+              \\   "threshold": "BLOCK_NONE",
+              \\ },
+              \\ {
+              \\   "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+              \\   "threshold": "BLOCK_NONE",
+              \\ }
+              \\ ]
+        let g:futago_generation_config = {
+              \\   "temperature": 0.9,
+              \\   "maxOutputTokens": 256,
+              \\ }
+      `,
     },
   },
 ];
