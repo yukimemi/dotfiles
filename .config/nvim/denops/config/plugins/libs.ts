4e40a30ca6f913ce206a53a1302b92baa062d635
GitHub <noreply@github.com>
2024-02-07 06:02:32 +0000

    chore(deps): Update deno dependencies

--- .config/nvim/denops/config/plugins/libs.ts
+++ .config/nvim/denops/config/plugins/libs.ts
@@ -10,7 +10,7 @@ import * as autocmd from "https://deno.land/x/denops_std@v6.0.1/autocmd/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v6.0.1/function/mod.ts";
 import * as mapping from "https://deno.land/x/denops_std@v6.0.1/mapping/mod.ts";
 import * as vars from "https://deno.land/x/denops_std@v6.0.1/variable/mod.ts";
-import { ensure, is } from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
+import { ensure, is } from "https://deno.land/x/unknownutil@v3.15.0/mod.ts";
 import { exists } from "https://deno.land/std@0.214.0/fs/mod.ts";
 import { pluginStatus } from "../pluginstatus.ts";
 
