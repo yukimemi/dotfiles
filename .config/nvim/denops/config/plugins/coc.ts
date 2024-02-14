ee6218918e3f5c3ec8033c6c3b771dcd3c6fd20e
GitHub <noreply@github.com>
2024-02-14 06:03:16 +0000

    chore(deps): Update deno dependencies

--- .config/nvim/denops/config/plugins/coc.ts
+++ .config/nvim/denops/config/plugins/coc.ts
@@ -12,7 +12,7 @@ import * as lambda from "https://deno.land/x/denops_std@v6.0.1/lambda/mod.ts";
 import * as mapping from "https://deno.land/x/denops_std@v6.0.1/mapping/mod.ts";
 import * as op from "https://deno.land/x/denops_std@v6.0.1/option/mod.ts";
 import * as vars from "https://deno.land/x/denops_std@v6.0.1/variable/mod.ts";
-import { ensure, is } from "https://deno.land/x/unknownutil@v3.16.0/mod.ts";
+import { ensure, is } from "https://deno.land/x/unknownutil@v3.16.1/mod.ts";
 import { pluginStatus } from "../pluginstatus.ts";
 import { execute } from "https://deno.land/x/denops_std@v6.0.1/helper/mod.ts";
 
