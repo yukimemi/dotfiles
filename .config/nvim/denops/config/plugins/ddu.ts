680d5761c093c1dc56d58d2e4508822a8c2a9817
GitHub <noreply@github.com>
2024-02-17 01:04:54 +0000

    chore(deps): Update deno dependencies

--- .config/nvim/denops/config/plugins/ddu.ts
+++ .config/nvim/denops/config/plugins/ddu.ts
@@ -12,7 +12,7 @@ import * as lambda from "https://deno.land/x/denops_std@v6.0.1/lambda/mod.ts";
 import * as mapping from "https://deno.land/x/denops_std@v6.0.1/mapping/mod.ts";
 import * as op from "https://deno.land/x/denops_std@v6.0.1/option/mod.ts";
 import { batch } from "https://deno.land/x/denops_std@v6.0.1/batch/mod.ts";
-import { ensure, is } from "https://deno.land/x/unknownutil@v3.16.1/mod.ts";
+import { ensure, is } from "https://deno.land/x/unknownutil@v3.16.3/mod.ts";
 import { notify } from "../util.ts";
 import { pluginStatus } from "../pluginstatus.ts";
 
