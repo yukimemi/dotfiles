a72f4e1719545b8abddb1803e886de111af3b36a
GitHub <noreply@github.com>
2024-04-12 06:02:45 +0000

    chore(deps): Update deno dependencies

--- .config/nvim/denops/config/option.ts
+++ .config/nvim/denops/config/option.ts
@@ -14,7 +14,7 @@ import * as nvimOption from "https://deno.land/x/denops_std@v6.4.0/option/nvim/m
 import * as option from "https://deno.land/x/denops_std@v6.4.0/option/mod.ts";
 import { batch } from "https://deno.land/x/denops_std@v6.4.0/batch/mod.ts";
 import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
-import { ensureDir } from "https://deno.land/std@0.222.0/fs/ensure_dir.ts";
+import { ensureDir } from "https://deno.land/std@0.222.1/fs/ensure_dir.ts";
 import { stdpath } from "https://deno.land/x/denops_std@v6.4.0/function/nvim/mod.ts";
 
 export async function setOption(denops: Denops) {
