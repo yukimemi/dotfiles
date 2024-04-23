d655806b1acd53df21a83d28db99c5e5833b9f78
GitHub <noreply@github.com>
2024-04-23 06:03:04 +0000

    chore(deps): Update deno dependencies

--- .config/nvim/denops/config/plugins/ddc.ts
+++ .config/nvim/denops/config/plugins/ddc.ts
@@ -11,7 +11,7 @@ import * as mapping from "https://deno.land/x/denops_std@v6.4.0/mapping/mod.ts";
 import * as vars from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
 import type { Plug } from "https://deno.land/x/dvpm@3.10.1/mod.ts";
 import { Denops } from "https://deno.land/x/denops_core@v6.0.6/mod.ts";
-import { z } from "https://deno.land/x/zod@v3.23.0/mod.ts";
+import { z } from "https://deno.land/x/zod@v3.23.3/mod.ts";
 import { exists } from "https://deno.land/std@0.223.0/fs/exists.ts";
 import { notify } from "../util.ts";
 import { pluginStatus } from "../pluginstatus.ts";
