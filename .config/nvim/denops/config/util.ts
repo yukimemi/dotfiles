87aa195d1bc45dc8450ae3f405f3fc92ad49fc19
GitHub <noreply@github.com>
2024-04-25 06:02:45 +0000

    chore(deps): Update deno dependencies

--- .config/nvim/denops/config/util.ts
+++ .config/nvim/denops/config/util.ts
@@ -5,14 +5,14 @@
 // =============================================================================
 
 import * as fn from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
-import * as fs from "https://deno.land/std@0.223.0/fs/mod.ts";
+import * as fs from "https://deno.land/std@0.224.0/fs/mod.ts";
 import * as helper from "https://deno.land/x/denops_std@v6.4.0/helper/mod.ts";
 import * as nvimFn from "https://deno.land/x/denops_std@v6.4.0/function/nvim/mod.ts";
 import * as option from "https://deno.land/x/denops_std@v6.4.0/option/mod.ts";
 import * as vars from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
 import type { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
 import { z } from "https://deno.land/x/zod@v3.23.4/mod.ts";
-import { join } from "https://deno.land/std@0.223.0/path/join.ts";
+import { join } from "https://deno.land/std@0.224.0/path/join.ts";
 import { systemopen } from "https://deno.land/x/systemopen@v1.0.0/mod.ts";
 
 const useNvimNotify = false;
