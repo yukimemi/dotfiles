d5929659d94fa47af9bf81ebb0afb00d4608d5ec
yukimemi <yukimemi@gmail.com>
2024-04-27 14:03:00 +0900

    feat: add option to center new floating windows       chore: migrate setup script from bat to powershell

---
+++ win/setup.ps1
@@ -0,0 +1,46 @@
+function Install-RequiredModules {
+  winget install -q gsudo
+  winget install -q RustLang.Rustup
+  winget install -q DenoLand.Deno
+  winget install -q Microsoft.WindowsTerminal
+  winget install -q Microsoft.PowerToys
+  winget install -q Git.Git
+  winget install -q GitHub.cli
+  winget install -q AutoHotkey.AutoHotkey
+  winget install -q Espanso.Espanso
+  winget install -q Starship.Starship
+  winget install -q Microsoft.VisualStudioCode
+  winget install -q Neovim.Neovim
+  winget install -q WinMerge.WinMerge
+  winget install -q SlackTechnologies.Slack
+  winget install -q Chocolatey.Chocolatey
+  winget install -q zig.zig
+  winget install -q GoLang.Go
+  winget install -q Microsoft.PowerShell
+  winget install -q Neovide.Neovide
+  winget install -q BurntSushi.ripgrep.MSVC
+  winget install -q sharkdp.bat
+  winget install -q junegunn.fzf
+  winget install -q ajeetdsouza.zoxide
+}
+
+function New-Shortcut {
+  param (
+    [string]$link,
+    [string]$target
+  )
+
+  $wsh = New-Object -ComObject WScript.Shell
+  $shortcut = $wsh.CreateShortcut($link)
+  $shortcut.TargetPath = $target
+  Write-Host "Created shortcut: ${link} -> ${target}"
+  $shortcut.Save()
+}
+
+Install-RequiredModules
+
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$target = Join-Path -Path $scriptDir -ChildPath "AutoHotkey\AutoHotkey.ahk"
+$link = Join-Path -Path $env:APPDATA -ChildPath "Microsoft\Windows\Start Menu\Programs\Startup\AutoHotkey.lnk"
+New-Shortcut -link $link -target $target
+
