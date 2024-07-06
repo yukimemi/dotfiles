959ea7f612122180beef8f07752148837cebaefb
yukimemi <yukimemi@gmail.com>
2024-07-06 20:57:01 +0900

    feat: Use starship instead of powerlevel10k. fix: disable os module from starship fix: disable instant prompt for p10k

--- .config/zsh/sync/prompt.zsh
+++ .config/zsh/sync/prompt.zsh
@@ -1,14 +1,14 @@
 # =============================================================================
 # File        : prompt.zsh
 # Author      : yukimemi
-# Last Change : 2023/09/13 00:12:43.
+# Last Change : 2024/07/06 20:44:09.
 # =============================================================================
 
-typeset -g POWERLEVEL9K_INSTANT_PROMPT=off
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+# typeset -g POWERLEVEL9K_INSTANT_PROMPT=off
+# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+# fi
+# [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-# eval "$(starship init zsh)"
+eval "$(starship init zsh)"
 
