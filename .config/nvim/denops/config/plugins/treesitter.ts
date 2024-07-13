4038bb3317f652d253ddd0b4dc2aea4120203f70
GitHub <noreply@github.com>
2024-07-13 14:23:51 +0000

    chore(deps): Update deno dependencies

--- .config/nvim/denops/config/plugins/treesitter.ts
+++ .config/nvim/denops/config/plugins/treesitter.ts
@@ -4,7 +4,7 @@
 // Last Change : 2024/06/16 15:58:53.
 // =============================================================================
 
-import type { Plug } from "https://deno.land/x/dvpm@3.14.7/mod.ts";
+import type { Plug } from "https://deno.land/x/dvpm@3.15.0/mod.ts";
 
 import * as mapping from "https://deno.land/x/denops_std@v6.5.1/mapping/mod.ts";
 import { pluginStatus } from "../pluginstatus.ts";
