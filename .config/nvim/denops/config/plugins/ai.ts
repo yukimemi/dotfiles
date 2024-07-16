02f42ace089061420c73ebdb0c50f9e7dc512a92
GitHub <noreply@github.com>
2024-07-16 06:03:32 +0000

    chore(deps): Update deno dependencies

--- .config/nvim/denops/config/plugins/ai.ts
+++ .config/nvim/denops/config/plugins/ai.ts
@@ -13,7 +13,7 @@ import {
   HarmBlockThreshold,
   HarmCategory,
   SafetySetting,
-} from "https://esm.sh/@google/generative-ai@0.14.1";
+} from "https://esm.sh/@google/generative-ai@0.15.0";
 
 export const ai: Plug[] = [
   { url: "https://github.com/kyoh86/denops-ollama.vim" },
