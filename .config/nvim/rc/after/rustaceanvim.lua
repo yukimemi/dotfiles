4ba420682ad2ea569d378be12193ffd69f761f85
yukimemi <yukimemi@gmail.com>
2024-09-14 23:33:24 +0900

    feat: update rustaceanvim settings
    Update default settings for rust-analyzer language server.

--- .config/nvim/rc/after/rustaceanvim.lua
+++ .config/nvim/rc/after/rustaceanvim.lua
@@ -1,7 +1,7 @@
 -- =============================================================================
 -- File        : rustaceanvim.lua
 -- Author      : yukimemi
--- Last Change : 2024/01/06 15:39:33.
+-- Last Change : 2024/09/14 23:17:48.
 -- =============================================================================
 
 vim.g.rustaceanvim = {
@@ -14,7 +14,7 @@ vim.g.rustaceanvim = {
       require("nvim-navic").attach(client, bufnr)
       require("lsp").on_attach(client, bufnr)
     end,
-    settings = {
+    default_settings = {
       -- rust-analyzer language server configuration
       ['rust-analyzer'] = {
         cargo = { allFeatures = true },
