7b4beefb420b8d3130cd372edb0a93e460569102
yukimemi <yukimemi@gmail.com>
2024-11-18 12:09:20 +0900

    fix(nvim): remove outdated vim-fall-source-mr dependency
    The `vim-fall-source-mr` plugin is no longer needed and has been removed from the dependencies.  This simplifies the configuration and removes an outdated dependency.

--- .config/nvim/denops/config/plugins/fall.ts
+++ .config/nvim/denops/config/plugins/fall.ts
@@ -1,18 +1,16 @@
 // =============================================================================
 // File        : fall.ts
 // Author      : yukimemi
-// Last Change : 2024/11/17 21:16:20.
+// Last Change : 2024/11/18 12:08:58.
 // =============================================================================
 
 import type { Plug } from "jsr:@yukimemi/dvpm@5.1.2";
 
 export const fall: Plug[] = [
-  { url: "https://github.com/lambdalisue/vim-fall-source-mr" },
   {
     url: "https://github.com/vim-fall/vim-fall",
     dependencies: [
       "https://github.com/vim-denops/denops.vim",
-      "https://github.com/lambdalisue/vim-fall-source-mr",
       "https://github.com/yukimemi/chronicle.vim",
     ],
     cache: { beforeFile: "~/.config/nvim/rc/before/fall.lua" },
