8b97cadaf026f31629969416b37aca94801d0afa
yukimemi <yukimemi@gmail.com>
2025-01-01 13:43:40 +0900

    feat: Install zoxide if not present
    This commit adds logic to install zoxide if it is not already present on the system. This ensures that the zoxide functionality is always available, even if it has not been installed by the user previously. It also evaluates the zoxide init command to enable the shell integration.

--- .config/zsh/defer/zoxide.zsh
+++ .config/zsh/defer/zoxide.zsh
@@ -1,8 +1,9 @@
 # =============================================================================
 # File        : zoxide.zsh
 # Author      : yukimemi
-# Last Change : 2023/09/16 00:43:28.
+# Last Change : 2025/01/01 13:42:52.
 # =============================================================================
 
+(( $+commands[zoxide] )) || curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 eval "$(zoxide init zsh)"
 
