5517ca793c0d2eef0a2e8cfdb5c0459226f97679
yukimemi <yukimemi@gmail.com>
2025-03-04 00:34:17 +0900

    feat: display file size and mtime in yazi
    This commit introduces a new linemode "size_and_mtime" to yazi, which displays both the file size and modification time.
    
    The following changes were made:
    
    - Added a `size_and_mtime` function to `init.lua` to format the file size and modification time.
    - Changed the default linemode in `yazi.toml` to `size_and_mtime`.
    - Changed keymap to `m` from `u` to avoid conflict.

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,7 +1,7 @@
 -- =============================================================================
 -- File        : init.lua
 -- Author      : yukimemi
--- Last Change : 2025/01/05 14:36:28.
+-- Last Change : 2025/03/02 23:53:11.
 -- =============================================================================
 
 -- https://github.com/hankertrix/augment-command.yazi
@@ -13,3 +13,17 @@ require("projects"):setup({})
 
 -- https://github.com/h-hg/yamb.yazi
 require("yamb"):setup({})
+
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+end
