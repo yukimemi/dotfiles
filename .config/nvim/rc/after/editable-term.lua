da7d45e316df0ef3cfc6405803b2d5a2582a87f1
yukimemi <yukimemi@gmail.com>
2025-04-20 21:52:05 +0900

    refactor(term): move windows pwsh terminal logic to keybinding
    Remove the global setting of `vim.o.shell` for Windows from the Lua cache.
    Explicitly specify `pwsh` when opening a terminal via the `<c-s>` keybinding, but only when running on Windows.

--- .config/nvim/rc/after/editable-term.lua
+++ .config/nvim/rc/after/editable-term.lua
@@ -1,17 +1,21 @@
 -- =============================================================================
 -- File        : editarble-term.lua
 -- Author      : yukimemi
--- Last Change : 2025/04/20 18:55:35.
+-- Last Change : 2025/04/20 21:51:12.
 -- =============================================================================
 
 local editableterm = require('editable-term')
 editableterm.setup({
   promts = {
     ['^%(gdb%) '] = {}, -- gdb promt
-    ['^>>> '] = {},     -- python PS1
-    ['^... '] = {},     -- python PS2
-    ['➤  '] = {},       -- starship
+    ['^>>> '] = {}, -- python PS1
+    ['^... '] = {}, -- python PS2
+    ['➤  '] = {}, -- starship
   },
 })
 
-vim.keymap.set("n", "<c-s>", "<cmd>terminal<cr>", { noremap = true, silent = true })
+if vim.fn.has('win32') == 1 then
+  vim.keymap.set("n", "<c-s>", "<cmd>terminal pwsh<cr>", { noremap = true, silent = true })
+else
+  vim.keymap.set("n", "<c-s>", "<cmd>terminal<cr>", { noremap = true, silent = true })
+end
