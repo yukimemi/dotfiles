257b6ecfe32b5a29592fd9508eae847660a6fc1d
yukimemi <yukimemi@gmail.com>
2025-11-29 01:59:35 +0900

    feat(nvim/drop): Configure seasonal themes
    Expand the 'drop' plugin setup to include various seasonal and holiday themes with specific date ranges.

--- .config/nvim/rc/after/drop.lua
+++ .config/nvim/rc/after/drop.lua
@@ -1,7 +1,23 @@
 -- =============================================================================
 -- File        : drop.lua
 -- Author      : yukimemi
--- Last Change : 2024/10/12 21:58:24.
+-- Last Change : 2025/11/29 01:25:18.
 -- =============================================================================
 
-require("drop").setup()
+require("drop").setup({
+  themes = {
+    { theme = "new_year",            from = { month = 12, day = 26 }, to = { month = 1, day = 10 } },
+    { theme = "valentines_day",      from = { month = 2, day = 1 },   to = { month = 2, day = 14 } },
+    { theme = "st_patricks_day",     from = { month = 3, day = 1 },   to = { month = 3, day = 14 } },
+    { theme = "easter",              holiday = "easter" },
+    { theme = "april_fools",         from = { month = 4, day = 1 },   to = { month = 4, day = 15 } },
+    { theme = "us_independence_day", month = 7,                       day = 4 },
+    { theme = "halloween",           from = { month = 10, day = 1 },  to = { month = 10, day = 31 } },
+    { theme = "us_thanksgiving",     holiday = "us_thanksgiving" },
+    { theme = "xmas",                from = { month = 11, day = 1 },  to = { month = 12, day = 25 } },
+    { theme = "leaves",              from = { month = 9, day = 22 },  to = { month = 9, day = 30 } },
+    { theme = "snow",                from = { month = 12, day = 26 }, to = { month = 3, day = 19 } },
+    { theme = "spring",              from = { month = 3, day = 20 },  to = { month = 6, day = 20 } },
+    { theme = "summer",              from = { month = 6, day = 21 },  to = { month = 9, day = 21 } },
+  },
+})
