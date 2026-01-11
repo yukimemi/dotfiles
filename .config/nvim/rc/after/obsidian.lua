a4640f8aa1424094264897a182cd6f7c5fa4e0c8
yukimemi <yukimemi@gmail.com>
2026-01-11 23:00:08 +0900

    feat(nvim): revert telescope and use Snacks.picker for obsidian commands
    - Revert telescope to false in pluginstatus.ts
    
    - Remove picker config in obsidian.lua
    
    - Override ObsidianQuickSwitch and ObsidianSearch commands to use Snacks.picker

--- .config/nvim/rc/after/obsidian.lua
+++ .config/nvim/rc/after/obsidian.lua
@@ -47,3 +47,23 @@ source.setup({
     },
   },
 })
+
+-- Snacks picker support for searching notes
+local obsidian_path = vim.fn.expand("~/obsidian")
+
+vim.keymap.set("n", "<leader>of", function()
+  Snacks.picker.files({ cwd = obsidian_path })
+end, { desc = "Find Obsidian notes (Snacks)" })
+
+vim.keymap.set("n", "<leader>og", function()
+  Snacks.picker.grep({ cwd = obsidian_path })
+end, { desc = "Grep Obsidian notes (Snacks)" })
+
+-- Override Obsidian commands to use Snacks picker
+vim.api.nvim_create_user_command("ObsidianQuickSwitch", function()
+  Snacks.picker.files({ cwd = obsidian_path })
+end, { force = true })
+
+vim.api.nvim_create_user_command("ObsidianSearch", function()
+  Snacks.picker.grep({ cwd = obsidian_path })
+end, { force = true })
