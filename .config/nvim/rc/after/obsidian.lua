70d22b9c3589b90fdf9ea35a213a3605f9c55813
yukimemi <yukimemi@gmail.com>
2026-01-11 20:49:05 +0900

    feat(nvim): add recommended mappings for obsidian.nvim
    - Configure <cr> for smart actions
    
    - Configure gf for link passthrough
    
    - Configure <leader>ch for checkbox toggle
    
    - Configure <leader>on for inserting links from selection

--- .config/nvim/rc/after/obsidian.lua
+++ .config/nvim/rc/after/obsidian.lua
@@ -16,4 +16,34 @@ source.setup({
     nvim_cmp = false,
     min_chars = 2,
   },
+  mappings = {
+    -- Smart action (links, tags, checkboxes)
+    ["<cr>"] = {
+      action = function()
+        return require("obsidian").util.smart_action()
+      end,
+      opts = { buffer = true, expr = true },
+    },
+    -- Overrides the 'gf' mapping to work on markdown/obsidian links within the vault.
+    ["gf"] = {
+      action = function()
+        return require("obsidian").util.gf_passthrough()
+      end,
+      opts = { noremap = false, expr = true, buffer = true },
+    },
+    -- Toggle check-boxes.
+    ["<leader>ch"] = {
+      action = function()
+        return require("obsidian").util.toggle_checkbox()
+      end,
+      opts = { buffer = true },
+    },
+    -- Create a new link from the visual selection
+    ["<leader>on"] = {
+      action = function()
+        return require("obsidian").util.insert_link()
+      end,
+      opts = { buffer = true },
+    },
+  },
 })
