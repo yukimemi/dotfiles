e4554918bf0523eb177fb0468f27ac7413c760ec
yukimemi <yukimemi@gmail.com>
2026-04-13 10:29:49 +0900

    refactor(lumiris): remove merged path from colors_path

--- dot_config/rvpm/plugins/github.com/yukimemi/lumiris.vim/before.lua
+++ dot_config/rvpm/plugins/github.com/yukimemi/lumiris.vim/before.lua
@@ -1,1 +1,1 @@
-vim.g["lumiris_colors_path"] = { vim.fn.expand("~/.cache/rvpm/merged"), vim.fn.expand("~/.cache/rvpm/repos") }
+vim.g["lumiris_colors_path"] = { vim.fn.expand("~/.cache/rvpm/repos") }
