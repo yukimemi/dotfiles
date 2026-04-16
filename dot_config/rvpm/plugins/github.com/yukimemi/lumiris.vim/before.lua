2d944b5ed0e7aa823eed7c14c177e79fc58dc4d2
yukimemi <yukimemi@gmail.com>
2026-04-16 11:11:40 +0900

    feat(plugins): add comment and contextment plugins, configure lumiris
    - Introduce `numToStr/Comment.nvim` for easy commenting
    - Add `uga-rosa/contextment

---
+++ dot_config/rvpm/plugins/github.com/yukimemi/lumiris.vim/before.lua
@@ -0,0 +1,8 @@
+vim.g["lumiris_colors_path"] = { vim.fn.expand("~/.cache/rvpm/repos") }
+vim.g["lumiris_notify"] = true
+vim.g["lumiris_interval"] = 600
+
+vim.keymap.set("n", "<space>co", "<cmd>ChangeColorscheme<cr>", { desc = "ChangeColorscheme" })
+vim.keymap.set("n", "<space>Rd", "<cmd>DisableThisColorscheme<cr>", { desc = "DisableThisColorscheme" })
+vim.keymap.set("n", "mL", "<cmd>LikeThisColorscheme<cr>", { desc = "LikeThisColorscheme" })
+vim.keymap.set("n", "mh", "<cmd>HateThisColorscheme<cr>", { desc = "HateThisColorscheme" })
