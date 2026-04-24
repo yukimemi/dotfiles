8df795c8d1cc6dc8d6dd62a94e6fe29941d2e685
yukimemi <yukimemi@gmail.com>
2026-04-24 09:10:04 +0900

    feat: add ahdr.vim, update todoke rules, and add glazewm excel rule

--- dot_config/rvpm/nvim/plugins/github.com/yukimemi/ahdr.vim/init.lua
+++ dot_config/rvpm/nvim/plugins/github.com/yukimemi/ahdr.vim/init.lua
@@ -1,8 +1,26 @@
 -- =============================================================================
 -- File        : init.lua
 -- Author      : yukimemi
--- Last Change : 2026/04/20 01:10:06.
+-- Last Change : 2026/04/24 09:28:55.
 -- =============================================================================
 
 vim.g.ahdr_debug = false
 vim.g.ahdr_cfg_path = "~/.config/ahdr/ahdr.toml"
+
+vim.api.nvim_create_user_command("DenopsAhdrDebug", function()
+  local group = vim.api.nvim_create_augroup("MyAhdr", { clear = true })
+  vim.api.nvim_create_autocmd("BufWritePost", {
+    group = group,
+    buffer = 0, -- <buffer>
+    command = "DenopsAhdr waitcmd",
+  })
+end, {})
+
+vim.api.nvim_create_user_command("DenopsAhdrPwshDebug", function()
+  local group = vim.api.nvim_create_augroup("MyAhdr", { clear = true })
+  vim.api.nvim_create_autocmd("BufWritePost", {
+    group = group,
+    buffer = 0, -- <buffer>
+    command = "DenopsAhdr waitcmdpwsh",
+  })
+end, {})
