3a8d3a2dcfdc32def73cb210a85248c8c517bfa9
yukimemi <yukimemi@gmail.com>
2026-05-25 09:14:11 +0900

    feat(nvim): add ccc.nvim plugin and update various configs
    - add `uga-rosa/ccc.nvim` for color code highlighting
    - switch `rvpm` ai provider to `gemini`
    - update `futago.vim` ai models to `gemini-2.5-flash`
    - configure `lumiris.vim` with `

--- home/.config/rvpm/nvim/plugins/github.com/yukimemi/futago.vim/init.lua
+++ home/.config/rvpm/nvim/plugins/github.com/yukimemi/futago.vim/init.lua
@@ -1,15 +1,15 @@
 -- =============================================================================
 -- File        : init.lua
 -- Author      : yukimemi
--- Last Change : 2026/04/20 01:10:06.
+-- Last Change : 2026/05/18 13:47:07.
 -- =============================================================================
 
 vim.g.futago_debug = false
 vim.g.futago_chat_path = "~/.cache/nvim/futago/chat"
 vim.g.futago_log_file = "~/.cache/nvim/futago/log/futago.log"
 vim.g.futago_history_db = "~/.cache/nvim/futago/db/history.db"
-vim.g.futago_model = "gemini-3-flash-preview"
-vim.g.futago_git_model = "gemini-3-flash-preview"
+vim.g.futago_model = "gemini-2.5-flash"
+vim.g.futago_git_model = "gemini-2.5-flash"
 vim.g.futago_safety_settings = {
   { category = "HARM_CATEGORY_HATE_SPEECH",       threshold = "BLOCK_NONE" },
   { category = "HARM_CATEGORY_SEXUALLY_EXPLICIT", threshold = "BLOCK_NONE" },
