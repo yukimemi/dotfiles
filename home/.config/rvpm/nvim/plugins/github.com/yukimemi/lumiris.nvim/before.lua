4edf980ecef1f2b2ebefb303a8c4fa35851ca3fe
yukimemi <yukimemi@gmail.com>
2026-06-05 09:46:39 +0900

    chore: update gitignore and clean nvim plugin config
    - `copyq`: generalize ignore pattern for `copyq-monitor.ini`
    - `nvim`: remove outdated comment about lumiris command model

--- home/.config/rvpm/nvim/plugins/github.com/yukimemi/lumiris.nvim/before.lua
+++ home/.config/rvpm/nvim/plugins/github.com/yukimemi/lumiris.nvim/before.lua
@@ -4,10 +4,6 @@
 -- Last Change : 2026/05/31 12:22:53
 -- =============================================================================
 
--- Keymaps migrated from lumiris.vim (before.lua). The command model changed:
---   lumiris.vim : Like / Hate (priority) + Disable (per-scheme exclude)
---   lumiris.nvim: Like (boost) / Hate (exclude a scheme), plus
---                 Enable / Disable / Toggle for the whole rotation.
 vim.keymap.set("n", "<space>co", "<cmd>LumirisChange<cr>", { desc = "lumiris: change colorscheme" })
 vim.keymap.set("n", "mL", "<cmd>LumirisLike<cr>", { desc = "lumiris: like this colorscheme" })
 vim.keymap.set("n", "mh", "<cmd>LumirisHate<cr>", { desc = "lumiris: hate (exclude) this colorscheme" })
